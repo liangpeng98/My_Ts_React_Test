6bcc7f5b8e9608644b5015df35b52607b01a5a78
lp01285521 <lp01285521@antgroup.com>
2022-08-23 13:59:02 +0800

    fix: fix page

--- src/pages/play/index.tsx
+++ src/pages/play/index.tsx
@@ -1,48 +1,44 @@
-import React, {
-  useState,
-  useEffect,
-  useRef,
-  useContext,
-  useReducer,
-} from 'react';
-import { Button } from 'antd';
-
+import React, { useState, useEffect, useRef, useContext, useReducer } from "react";
+import { Button } from "antd";
 
 /**
- * usePrevious 通过useRef获取上一轮的值
- * @param value 
- * @returns 
+ * @name usePrevious 通过useRef获取上一轮的值
+ * @param value
+ * @returns current 上一次的值
  */
-const usePrevious = (value: any) => {
-  const ref = useRef();
+const usePrevious = (value: number) => {
+  const ref: React.MutableRefObject<number> = useRef(0);
   useEffect(() => {
     ref.current = value;
-  });
+  }, [value]);
   return ref.current;
 };
-
 const Counter = (): JSX.Element => {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState<number>(0);
   const prevCount = usePrevious(count);
+
   return (
     <div>
-      <h1>
-        现在: {count}, 之前: {prevCount}
-      </h1>
+      <h1>之前：{prevCount}</h1>
+      <h1>现在: {count}</h1>
       <Button onClick={() => setCount((val) => val + 1)}>count+1按钮</Button>
     </div>
   );
 };
 
+/**
+ * @name
+ * @params
+ * @return
+ */
 const Example = (): JSX.Element => {
   const [count, setCount] = useState(0);
 
   function handleAlertClick() {
     setTimeout(() => {
-      alert('You clicked on: ' + count);
+      alert("You clicked on: " + count);
     }, 3000);
   }
-
   return (
     <div>
       <p>You clicked {count} times</p>
@@ -52,31 +48,18 @@ const Example = (): JSX.Element => {
   );
 };
 
-const Counters = () => {
-  const [count, setCount] = useState(0);
-  return (
-    <div>
-      Count: {count}
-      <Button onClick={() => setCount(0)}>Reset</Button>
-      <Button onClick={() => setCount((prevCount) => prevCount - 1)}>-</Button>
-      <Button onClick={() => setCount((prevCount) => prevCount + 1)}>+</Button>
-    </div>
-  );
-};
-
-
 /**
- * 使用 useReducer 
+ * 使用 useReducer
  */
 const ThemeContext = React.createContext<any>(null);
-const setCountFn = (state:any,action:any) => {
+const setCountFn = (state: any, action: any) => {
   switch (action.type) {
-    case 'before':
-      return state
-    case 'now' : 
-    return {count: state.count+1}
+    case "before":
+      return state;
+    case "now":
+      return { count: state.count + 1 };
     default:
-      return state
+      return state;
   }
   return { count: 1 };
 };
@@ -105,8 +88,8 @@ const ThemedButton = (props: any): JSX.Element => {
   const dispatch = useContext(ThemeContext);
   return (
     <>
-      <Button onClick={()=> dispatch({type: 'before'})}>before: {props.state.count}</Button>
-      <Button onClick={()=> dispatch({type: 'now'})}>now: {props.state.count}</Button>
+      <Button onClick={() => dispatch({ type: "before" })}>before: {props.state.count}</Button>
+      <Button onClick={() => dispatch({ type: "now" })}>now: {props.state.count}</Button>
     </>
   );
 };
@@ -115,11 +98,9 @@ export default function Play(): JSX.Element {
   return (
     <div>
       <div>我在打游戏</div>
-      <Counter></Counter>
+      <Counter />
       <br />
       <Example />
-      <br />
-      <Counters></Counters>
       <br />
       <App></App>
     </div>
--- src/pages/team/index.tsx
+++ src/pages/team/index.tsx
@@ -1,34 +1,46 @@
 // 1. 引入包
-import React, { useState, useEffect } from "react"
+import React, { useState, useEffect } from 'react';
 
-import { Form, Input, Button, Radio } from "antd"
+import { Form, Input, Button, Radio } from 'antd';
 
-import styles from "./index.module.scss"
+import styles from './index.module.scss';
 
 // 2. 声明组件
 interface IFormData {
-  name: string
-  typeName: string
+  name: string;
+  typeName: string;
 }
+
 const Header = (): JSX.Element => {
-  const [form] = Form.useForm()
+  const [form] = Form.useForm();
 
   const formItemLayout = {
     labelCol: { span: 8 },
-    wrapperCol: { span: 16 }
-  }
+    wrapperCol: { span: 16 },
+  };
 
   const buttonItemLayout = {
-    wrapperCol: { span: 14, offset: 10 }
-  }
+    wrapperCol: { span: 14, offset: 10 },
+  };
 
   const onFinishHandle = (values: IFormData) => {
-    console.log("查询条件", values)
-  }
+    console.log('查询条件', values);
+  };
 
   const onFinishFailedHandle = (errorInfo: object) => {
-    console.log("失败", errorInfo)
-  }
+    console.log('失败', errorInfo);
+  };
+
+  const [isShow, setIsShow] = useState(Boolean);
+  useEffect(() => {
+    if (
+      /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini|Mobile|mobile/i.test(window.navigator.userAgent)
+    ) {
+      setIsShow(true);
+    } else {
+      setIsShow(false);
+    }
+  }, []);
 
   return (
     <div className={styles.headerBox}>
@@ -52,16 +64,35 @@ const Header = (): JSX.Element => {
         </Form.Item>
       </Form>
     </div>
-  )
+  );
+};
+function Counter() {
+  const [count, setCount] = useState(0);
+  const [step, setStep] = useState(1);
+
+  useEffect(() => {
+    const timerID = setInterval(() => {
+      setCount(count + step);
+    }, 1000);
+    return () => clearInterval(timerID);
+  }, [count, step]);
+
+  return (
+    <>
+      <h1>{count}</h1>
+      <Input value={step} onChange={(e) => setStep(Number(e.target.value))} />
+    </>
+  );
 }
 
 const Team = (): JSX.Element => {
   return (
     <div>
       <Header />
+      <Counter />
     </div>
-  )
-}
+  );
+};
 
 // 导出组件
-export default Team
+export default Team;
